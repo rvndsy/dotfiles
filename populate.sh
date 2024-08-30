b0b4536026c7c6e7e8d0881cf85482fad5e12e68
probablyalright <112282114+probablyalright@users.noreply.github.com>
2024-08-30 11:18:07 +0300

    Add functional script

--- populate.sh
+++ populate.sh
@@ -1,12 +1,41 @@
 #!/bin/bash
+# DEPENDENCIES:
+# - tee
 
-echo "> Removing existing ~/.bashrc ...";
-cp ~/.bashrc ~/.dotfiles/removed &&
-rm ~/.bashrc;
+# Location of .dotfiles and other necessities within
+ROOTDIR="$HOME/.dotfiles";
+LOGDIR="$ROOTDIR/log";
+RMDIR="$ROOTDIR/removed";
+
+# Actual dotfile system and repo directory locations go here
+BASHRC="~/.bashrc";
+DOTBASHRC="$ROOTDIR/.bashrc";
+
+# Datetime for logging purposes
+DATE=$(date +%Y-%m-%d-%H-%M-%S-%N);
+
+# Start logging
+LOG="$LOGDIR/populate-$DATE";
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>$LOG 2>&1
+touch $LOG;
+
+## Populating .bashrc
+echo "$BASHRC > Removing existing ...";
+cp $BASHRC $RMDIR && rm $BASHRC;
 if [ $? -eq 0 ]; then
-    echo "> Successfully removed existing ~/.bashrc";
+    echo "$BASHRC > Successfully removed existing ";
 else 
-    echo "> Failed removing existing ~/.bashrc";
-    echo "  Does symboling link already exist?";
-echo "> Populating ~/.bashrc"
-ln -s ~/.dotfiles/.bashrc ~/.bashrc;
+    echo "$BASHRC > Failed removing existing";
+    echo "$BASHRC >> Does symboling link already exist? Did you remove the file?";
+fi
+
+echo "$BASHRC > Distributing dotfile...";
+ln -s $DOTBASHRC $BASHRC;
+if [ $? -eq 0 ]; then
+    echo "$BASHRC > Successfully populated";
+else 
+    echo "$BASHRC > Failed to populate";
+fi
+
